604760edef7217cf178acd05486419bb7a378bb5
David L. Inman <davidlevere@gmail.com>
2021-08-14 00:02:43 -0400

    grammar inc

--- Parsing/Grammar.cs
+++ Parsing/Grammar.cs
@@ -1,4 +1,13 @@
 
-namespace sand.Parsing {
+using sand.Util;
+using static sand.Util.ResultEx;
+using static sand.Util.OptionEx;
 
+namespace sand.Parsing {
+    public class Grammar {
+
+        private Parser<Expr> ParseExpr() {
+            return null;
+        }
+    }
 }
