27789b433fa64bde464f1130335070dbc901af51
David L. Inman <davidlevere@gmail.com>
2021-08-14 00:02:19 -0400

    ast literals

--- Parsing/Ast.cs
+++ Parsing/Ast.cs
@@ -2,5 +2,10 @@
 using sand.Util;
 
 namespace sand.Parsing {
-    
+    public interface Expr { }
+
+    public record Integer(int i) : Expr;
+    public record Str(string s) : Expr;
+    public record Boolean(bool b) : Expr;
+
 }
