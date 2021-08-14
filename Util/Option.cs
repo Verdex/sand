7f2f48ffad941fb57a59b3896889190468d2f40c
David L. Inman <davidlevere@gmail.com>
2021-08-13 22:27:42 -0400

    map for option and result

--- Util/Option.cs
+++ Util/Option.cs
@@ -1,4 +1,6 @@
 
+using System;
+ 
 namespace sand.Util {
     public interface Option<T> { }
 
@@ -14,5 +16,11 @@ namespace sand.Util {
     public static class OptionEx {
         public static Option<T> Some<T>( T item ) => new Some<T>(item);
         public static Option<T> None<T>() => new None<T>();
+        public static Option<B> Map<A, B>(this A target, Func<A, B> f) 
+            => target switch {
+                Some<A> o => Some(f(o.Item)),
+                None<A> => None<B>(),
+                _ => throw new Exception("Unexpected Option case"),
+            };
     }
 }
--- Util/Result.cs
+++ Util/Result.cs
@@ -1,3 +1,6 @@
+
+using System;
+
 namespace sand.Util {
     public interface Result<T> { }
 
@@ -18,5 +21,11 @@ namespace sand.Util {
     public static class ResultEx {
         public static Result<T> Ok<T>(T item) => new Ok<T>(item);
         public static Err<T> Err<T>(Error error) => new Err<T>(error);
+        public static Result<B> Map<A, B>(this A target, Func<A, B> f) 
+            => target switch {
+                Ok<A> o => Ok(f(o.Item)),
+                Err<A> e => Err<B>(e.Error),
+                _ => throw new Exception("Unexpected Result case"),
+            };
     }
 }
