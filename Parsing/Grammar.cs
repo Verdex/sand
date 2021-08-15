7bcecee81390188b78720c5109caf1775604e284
David L. Inman <davidlevere@gmail.com>
2021-08-14 23:08:21 -0400

    IntegerParser definition

--- Parsing/Grammar.cs
+++ Parsing/Grammar.cs
@@ -1,10 +1,22 @@
 
+using System;
+using System.Linq;
+
 using sand.Util;
+using static sand.Parsing.ParserEx;
 using static sand.Util.ResultEx;
 using static sand.Util.OptionEx;
 
 namespace sand.Parsing {
     public class Grammar {
+
+        private Parser<Integer> IntegerParser()  
+            => (from c in Any()
+               where char.IsNumber(c)
+               select c).OneoOrMore()
+                        .Select(cs => new string(cs.ToArray()))
+                        .Select(str => int.Parse(str))
+                        .Select( i => new Integer(i) );
 
         private Parser<Expr> ParseExpr() {
             return null;
