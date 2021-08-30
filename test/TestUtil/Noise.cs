491357e9957c2c59f7a0f92b8245a8c1cbb84dee
David L. Inman <davidlevere@gmail.com>
2021-08-29 22:36:45 -0400

    noise inc prog

--- test/TestUtil/Noise.cs
+++ test/TestUtil/Noise.cs
@@ -1,5 +1,23 @@
 namespace test.TestUtil {
-    public class Noise {
+    public record Noise(ulong seed) {
 
+        public ulong Any() {
+            var x = seed + 17;
+            
+            x ^= x << 13;
+            x ^= x >> 7;
+            x ^= x << 17;
+
+            return x;
+        }
+
+        public Noise Next() => this with {seed = this.Any()};
+
+        public ulong InRange(ulong min, ulong max) {
+            var m = (max - min) + 1;
+            return seed % m + min;
+        }
+
+        public ulong Max(ulong max) => InRange(0, max);
     }
 }
